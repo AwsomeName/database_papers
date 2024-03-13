d6a5f05bbace560be2467c9a55931162a813c281
AwsomeName <liuchang_hit_cs@gmail.com>
2024-03-13 16:21:05 +0800

    update

--- kg/insert_neo4j.py
+++ kg/insert_neo4j.py
@@ -2,13 +2,26 @@ from py2neo import Graph
 from py2neo import Node, Relationship
 import json
 from tqdm import tqdm
+from retrying import retry
+import time
 
-graph = Graph("http://192.168.31.8://7474", auth=("neo4j", 'abc12345'), name="kg_ver_0_1")
+# graph = Graph("http://192.168.31.8:7474", auth=("neo4j", 'abc12345'), name="kg_ver_0_1")
+# graph = Graph("http://localhost:7474", auth=("neo4j", 'abc12345'), name="kg_ver_0_1")
+graph = Graph("http://localhost:7474", password='abc12345', name="neo4j")
 print("graph:", graph)
 
+@retry(stop_max_attempt_number=3, wait_fixed=10)
+def create_node(e_node):
+    graph.create(e_node)
+
+
 print("process and inserting ... ")
+cnt = 0 
 with open("/home/lc/data/all_entities.txt", 'r') as fp:
     for line in tqdm(fp.readlines()):
+        cnt += 1
+        if cnt < 2426:
+            continue
         line = line.strip()
         e_id, e_info = json.loads(line)
         e_node = Node(
@@ -23,7 +36,11 @@ with open("/home/lc/data/all_entities.txt", 'r') as fp:
         del e_info['alias_name']
         del e_info['spec']
         for key in e_info:
+            if key == "":
+                continue
             e_node[key] = e_info[key]
             
-        graph.create(e_node)
+        create_node(e_node)
+        # graph.create(e_node)
+        # break
         
